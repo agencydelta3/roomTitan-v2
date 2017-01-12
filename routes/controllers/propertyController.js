d81c27a99c9443d65f8241b5670ef6a9cb99b5e1
agencydelta4 <agencydelta4@gmail.com>
2017-01-12 18:19:35 +0600

    End of day - 12th Jan

--- routes/controllers/propertyController.js
+++ routes/controllers/propertyController.js
@@ -1,5 +1,9 @@
 module.exports.controller = function (app) {
-    app.get('/member-property-management', function (req, res, next) {
-        res.render('pages/member/property/propertyManagement');
-    });
+  app.get('/member-property-management', function (req, res, next) {
+      res.render('pages/member/property/propertyManagement');
+  });
+
+  app.get('/property/manage', function (req, res) {
+      res.render('pages/propertyManagement/manageProperty');
+  });
 };
