e371e59f4a7062798a6ed94e968c9a8920afa9dd
agencydelta4 <agencydelta4@gmail.com>
2017-01-16 17:26:14 +0600

    trying

--- routes/controllers/propertyController.js
+++ routes/controllers/propertyController.js
@@ -1,9 +1,8 @@
 module.exports.controller = function (app) {
-  app.get('/member-property-management', function (req, res, next) {
-      res.render('pages/member/property/propertyManagement');
-  });
-
-  app.get('/property/manage', function (req, res) {
+  app.get('/property/manage', function (req, res, next) {
       res.render('pages/propertyManagement/manageProperty');
+  });
+  app.get('/property/manage/roommate', function (req, res, next) {
+      res.render('pages/member/property/propertyManagement');
   });
 };
