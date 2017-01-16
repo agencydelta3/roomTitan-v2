43dae79da11f9e13f058f192f9f4fdabe70c0327
agencydelta4 <agencydelta4@gmail.com>
2017-01-16 17:29:48 +0600

    Done trying

--- routes/controllers/propertyController.js
+++ routes/controllers/propertyController.js
@@ -1,7 +1,8 @@
 module.exports.controller = function (app) {
-  app.get('/property/manage', function (req, res, next) {
-      res.render('pages/propertyManagement/manageProperty');
+  app.get('/member-property-management', function (req, res, next) {
+      res.render('pages/member/property/addEdit/propertyManagement');
   });
+
   app.get('/property/manage/roommate', function (req, res, next) {
       res.render('pages/member/property/propertyManagement');
   });
