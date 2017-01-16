da3bc3fc5272a625c0ec0b39652d5a297b719c0d
Mazba Uddin <mazba09@gmail.com>
2017-01-16 17:25:40 +0600

    sync

--- routes/controllers/propertyController.js
+++ routes/controllers/propertyController.js
@@ -1,6 +1,6 @@
 module.exports.controller = function (app) {
   app.get('/member-property-management', function (req, res, next) {
-      res.render('pages/member/property/propertyManagement');
+      res.render('pages/member/property/addEdit/propertyManagement');
   });
 
   app.get('/property/manage', function (req, res) {
