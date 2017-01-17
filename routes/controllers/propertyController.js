db8d52c19378eec1a41e995905bb63077193bc6c
agencydelta4 <agencydelta4@gmail.com>
2017-01-17 11:10:11 +0600

    noUiSlider

--- routes/controllers/propertyController.js
+++ routes/controllers/propertyController.js
@@ -3,7 +3,7 @@ module.exports.controller = function (app) {
       res.render('pages/member/property/addEdit/propertyManagement');
   });
 
-  app.get('/property/manage/roommate', function (req, res, next) {
-      res.render('pages/member/property/propertyManagement');
+  app.get('/property/manage', function (req, res, next) {
+      res.render('pages/propertyManagement/manageProperty');
   });
 };
