a466e8431ee8a7f5e17b78f4afed539486dbfab9
Abir Roy <agencydelta4@gmail.com>
2017-01-31 13:18:10 +0600

    Add Expenses - Mobile View

--- routes/controllers/propertyController.js
+++ routes/controllers/propertyController.js
@@ -2,6 +2,10 @@ module.exports.controller = function (app) {
   app.get('/member-property-management', function (req, res, next) {
       res.render('pages/member/property/addEdit/propertyManagement');
   });
+  
+  app.get('/add-expenses', function (req, res, next) {
+      res.render('pages/member/property/addEdit/addPropertyExpenses');
+  });
 
   app.get('/property/manage', function (req, res, next) {
       res.render('pages/propertyManagement/manageProperty');
